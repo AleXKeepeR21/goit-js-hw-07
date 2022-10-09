215b5d22ded6c91ead142b8608340d1373f75f15
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-10-09 11:12:31 +0300

    дз1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+      .join("");
+}
+
+// console.log(galleryItems);
+
+galleryMarkup.addEventListener('click', onGalleryRefClick);
+
+let modalWindow;
+
+function onGalleryRefClick(event) {
+    event.preventDefault();
+    const isGalleryRef = event.target.classList.contains('.gallery__image');
+    if (!isGalleryRef) {
+        return
+    }
+    console.log(event.target);
+
+    modalWindow = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+    modalWindow.show();
+}
